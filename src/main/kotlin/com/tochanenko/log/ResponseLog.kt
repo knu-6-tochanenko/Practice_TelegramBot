5bad1dea459c0b45e0079291ee14bf884fe46815
Vladyslav Tochanenko <itochanenko@gmail.com>
2023-04-06 16:04:55 +0300

    Remove file writing for Heroku

--- src/main/kotlin/com/tochanenko/log/ResponseLog.kt
+++ src/main/kotlin/com/tochanenko/log/ResponseLog.kt
@@ -1,5 +1,6 @@
 package com.tochanenko.log
 
+import com.tochanenko.tools.parseEnvVar
 import eu.vendeli.tgbot.types.User
 import java.nio.file.Files
 import java.nio.file.Paths
@@ -7,13 +8,15 @@ import java.text.SimpleDateFormat
 import java.util.*
 
 fun log(user: User, input: String, output: String) {
-    Files.createDirectories(Paths.get("/out/"));
-    val time = System.currentTimeMillis()
-    val date = Date(time)
-    val sdf = SimpleDateFormat("dd MMMM yyyy HH:mm:ss")
-    java.io.File("./out/$time-${user.username}.txt").writeText(
-        "Time: ${sdf.format(date)}\n\nName: ${user.firstName} ${user.lastName} (${user.username})\n\n" +
-                "Request:\n$input\n\n" +
-                "Response:\n$output"
-    )
+    if (parseEnvVar("HEROKU_TOKEN").isNotEmpty()) {
+        Files.createDirectories(Paths.get("/out/"));
+        val time = System.currentTimeMillis()
+        val date = Date(time)
+        val sdf = SimpleDateFormat("dd MMMM yyyy HH:mm:ss")
+        java.io.File("./out/$time-${user.username}.txt").writeText(
+            "Time: ${sdf.format(date)}\n\nName: ${user.firstName} ${user.lastName} (${user.username})\n\n" +
+                    "Request:\n$input\n\n" +
+                    "Response:\n$output"
+        )
+    }
 }
