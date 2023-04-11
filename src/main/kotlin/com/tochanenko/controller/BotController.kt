7be73c43e2925119dc6360a571195223215c7fd1
Vladyslav Tochanenko <itochanenko@gmail.com>
2023-04-11 20:26:48 +0300

    Refactor commands

--- src/main/kotlin/com/tochanenko/controller/BotController.kt
+++ src/main/kotlin/com/tochanenko/controller/BotController.kt
@@ -12,11 +12,16 @@ import eu.vendeli.tgbot.api.message
 import eu.vendeli.tgbot.types.ParseMode
 import eu.vendeli.tgbot.types.internal.ProcessedUpdate
 
+const val START = "/start"
+const val ABOUT = "/about"
+const val COMMANDS = "/commands"
+const val CHAT = "/chat"
+
 const val helloMessage: String = "*Привіт!*\n\n" +
         "Я - бот, який допоможе тобі дізнатись кількість калорій в страві " +
         "за її інгредієнтами та їх кількості в грамах або в інших способах " +
         "вимірювання ваги/об'єму: чайні ложки, столові ложки.\n\n" +
-        "Просто напиши інгредієнти та їх кількість і чекай на відповідь!\n\n"
+        "Просто напиши інгредієнти та їх кількість і чекай на відповідь!"
 
 const val aboutMessage: String = "$helloMessage\n\n" +
         "Мене розробив *Владислав Точаненко* у 2023 році. Я є частиною виробничої " +
@@ -24,39 +29,44 @@ const val aboutMessage: String = "$helloMessage\n\n" +
         "наук та кібернетики, КНУ ім Т. Шевченка. Якщо у Вас з'явились якісь питання, " +
         "можете звертатись до @tochanenko.\n\nВерсія: 1.0"
 
-const val commandsMessage: String = "/about - Дізнатись більше про цей бот і про " +
+const val commandsMessage: String = "$ABOUT - Дізнатись більше про цей бот і про " +
         "його власника\n" +
-        "/commands - Список усіх доступних команд\n" +
+        "$COMMANDS - Список усіх доступних команд\n" +
         "Будь-який текст, що містить інгредієнти та їх кількість - Список інгредієнтів " +
         "з їх калорійністю та сумарною калорійністю страви"
 
+const val chatGPTMessage: String = "Введіть посилання до ChatGPT в одному повідомленні з командою $CHAT"
+
+const val unknownIngredientsMessage: String = "Вибачте, я Вас не зрозумів. Можливо, Ви написали " +
+        "текст, що не містить інгредієнтів. Якщо це помилка Бота, зверніться до @tochanenko"
+
 class BotController {
-    @CommandHandler(["/start"])
+    @CommandHandler([START])
     suspend fun start(update: ProcessedUpdate, bot: TelegramBot) {
         message { helloMessage }
             .options { parseMode = ParseMode.Markdown }
             .send(update.user, bot)
     }
 
-    @CommandHandler(["/about"])
+    @CommandHandler([ABOUT])
     suspend fun about(update: ProcessedUpdate, bot: TelegramBot) {
         message { aboutMessage }
             .options { parseMode = ParseMode.Markdown }
             .send(update.user, bot)
     }
 
-    @CommandHandler(["/commands"])
+    @CommandHandler([COMMANDS])
     suspend fun commands(update: ProcessedUpdate, bot: TelegramBot) {
         message { commandsMessage }
             .options { parseMode = ParseMode.Markdown }
             .send(update.user, bot)
     }
 
-    @CommandHandler(["/t"])
+    @CommandHandler([CHAT])
     suspend fun chatGPT(update: ProcessedUpdate, bot: TelegramBot) {
         val message = update.text!!.toString()
         val response = if (message.length > 2) chatGPTAnswer(userInput = message)
-        else "Введіть посилання до ChatGPT в одному повідомленні з командою /t"
+        else chatGPTMessage
         message { response }.send(update.user, bot)
     }
 
@@ -64,13 +74,12 @@ class BotController {
     suspend fun saySomething(update: ProcessedUpdate, bot: TelegramBot) {
         val typingAction = TypingAction(update.user.id, bot).start()
 
-
         val ingredients: List<String>? = update.text?.let { getIngredients(it) }
 
         if (ingredients != null) {
             if (ingredients.isEmpty() || ingredients[0].contains("устий список")) {
                 typingAction.stop()
-                message { "Вибачте, я Вас не зрозумів. Можливо, Ви написали текст, що не містить інгредієнтів. Якщо це помилка Бота, зверніться до @tochanenko" }.send(
+                message { unknownIngredientsMessage }.send(
                     update.user,
                     bot
                 )
@@ -94,6 +103,5 @@ class BotController {
                 message { response }.options { parseMode = ParseMode.Markdown }.send(update.user, bot)
             }
         }
-
     }
 }
